bd609e180a4354fe79ddfc7be471136f22dbb3b5
Filip Mróz <fafafft@gmail.com>
2021-04-18 23:40:05 +0200

    easier working with jupyter examples

--- sep/inspect/start.py
+++ sep/inspect/start.py
@@ -1,14 +1,27 @@
-import pathlib
-
+import fire
 import os
+import pathlib
+import shutil
 
-# TODO option to copy the notebook to new folder (e.g. where the project is happening)
 
-if __name__ == '__main__':
-    inspect_dir = str(pathlib.Path(__file__).parent)
+def _jupyter(inspect_dir):
     command = f'cd {inspect_dir}'
     print("Starting jupyter inside:", command)
     os.system(command)
     os.chdir(inspect_dir)
     command = f'jupyter notebook'
     os.system(command)
+
+
+def here():
+    inspect_dir = str(pathlib.Path(__file__).parent)
+    _jupyter(inspect_dir)
+
+
+def from_copy(path_to_copy_inspect_to):
+    shutil.copytree(pathlib.Path(__file__).parent, path_to_copy_inspect_to)
+    _jupyter(path_to_copy_inspect_to)
+
+
+if __name__ == '__main__':
+    fire.Fire()
